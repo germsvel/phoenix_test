8d3b5088671a0c7ad9832f10464354e2842c3b40
German Velasco <germsvel@gmail.com>
2024-01-07 12:10:17 -0500

    Add first tests for visit and assert_has/3

--- test/phoenix_test_test.exs
+++ test/phoenix_test_test.exs
@@ -16,11 +16,53 @@ defmodule PhoenixTestTest do
   end
 
   describe "click_link/2" do
-    test "navigates to path", %{conn: conn} do
+    test "follows link's path", %{conn: conn} do
       conn
       |> visit("/index")
       |> click_link("Page 2")
       |> assert_has("h1", "Page 2")
+    end
+
+    test "follows first link when there are multiple links with same text", %{conn: conn} do
+      conn
+      |> visit("/index")
+      |> click_link("Multiple links")
+      |> assert_has("h1", "Page 3")
+    end
+  end
+
+  describe "assert_has/3" do
+    test "returns true if a single element is found with CSS selector and text", %{conn: conn} do
+      conn =
+        conn
+        |> visit("/index")
+
+      conn |> assert_has("h1", "Main page")
+      conn |> assert_has("#title", "Main page")
+      conn |> assert_has(".title", "Main page")
+      conn |> assert_has("[data-role='title']", "Main page")
+    end
+
+    test "raises an error if the element cannot be found", %{conn: conn} do
+      conn =
+        conn
+        |> visit("/index")
+
+      assert_raise RuntimeError, ~s(unable to find element with selector "#nonexistent-id"), fn ->
+        conn |> assert_has("#nonexistent-id", "Main page")
+      end
+    end
+
+    test "raises an error if more than one element is found", %{conn: conn} do
+      conn =
+        conn
+        |> visit("/index")
+
+      assert_raise RuntimeError,
+                   ~s(found more than one element with selector ".multiple_links"),
+                   fn ->
+                     conn |> assert_has(".multiple_links", "Multiple links")
+                   end
     end
   end
 end
