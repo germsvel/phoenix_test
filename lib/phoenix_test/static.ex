5aa3d73a2595fe8a5819bd4a4de6acfcf33fce7a
German Velasco <germsvel@gmail.com>
2024-01-15 11:49:04 -0500

    Separate the defimpl from parent module. Easier to read

--- lib/phoenix_test/static.ex
+++ lib/phoenix_test/static.ex
@@ -2,29 +2,31 @@ defmodule PhoenixTest.Static do
   @endpoint Application.compile_env(:phoenix_test, :endpoint)
   import Phoenix.ConnTest
 
-  alias PhoenixTest.Html
-
   defstruct [:conn]
 
   def visit(conn, path) do
     %__MODULE__{conn: get(conn, path)}
   end
+end
 
-  defimpl PhoenixTest.Driver, for: PhoenixTest.Static do
-    def click_link(session, text) do
-      path =
-        session
-        |> render_html()
-        |> Html.parse()
-        |> Html.find("a", text)
-        |> Html.attribute("href")
+defimpl PhoenixTest.Driver, for: PhoenixTest.Static do
+  import Phoenix.ConnTest
 
-      PhoenixTest.visit(session.conn, path)
-    end
+  alias PhoenixTest.Html
 
-    def render_html(%{conn: conn}) do
-      conn
-      |> html_response(200)
-    end
+  def click_link(session, text) do
+    path =
+      session
+      |> render_html()
+      |> Html.parse()
+      |> Html.find("a", text)
+      |> Html.attribute("href")
+
+    PhoenixTest.visit(session.conn, path)
+  end
+
+  def render_html(%{conn: conn}) do
+    conn
+    |> html_response(200)
   end
 end
