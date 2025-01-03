1e2d910780576b0676337600aeb804e1a3d7ab0b
German Velasco <germsvel@gmail.com>
2025-01-03 13:38:46 -0500

    Test live_view_redirect message in watcher

--- test/phoenix_test/live_view_watcher_test.exs
+++ test/phoenix_test/live_view_watcher_test.exs
@@ -11,7 +11,16 @@ defmodule PhoenixTest.LiveViewWatcherTest do
     end
 
     def init(opts) do
+      if opts[:redirect_in] do
+        Process.send_after(self(), :redirect, opts[:redirect_in])
+      end
+
       {:ok, opts}
+    end
+
+    def handle_info(:redirect, state) do
+      reason = {:shutdown, {:redirect, %{}}}
+      {:stop, reason, state}
     end
   end
 
@@ -53,26 +62,39 @@ defmodule PhoenixTest.LiveViewWatcherTest do
     assert_receive {:watcher, :live_view_died}
   end
 
-  test "sends :timeout message when LiveView timeout expires" do
-    {:ok, view_pid} = start_supervised({DummyLiveView, []})
-    {:ok, proxy_pid} = start_supervised({DummyLiveViewTestProxy, []})
-    view = %{pid: view_pid, proxy: fake_proxy(proxy_pid)}
-    {:ok, watcher} = start_supervised({LiveViewWatcher, %{view: view, caller: self()}})
+  describe "watch_view/2" do
+    test "sends :live_view_redirected message when LiveView redirects" do
+      {:ok, view_pid} = start_supervised({DummyLiveView, %{redirect_in: 10}})
+      {:ok, proxy_pid} = start_supervised({DummyLiveViewTestProxy, []})
+      view = %{pid: view_pid, proxy: fake_proxy(proxy_pid)}
+      {:ok, watcher} = start_supervised({LiveViewWatcher, %{view: view, caller: self()}})
 
-    :ok = LiveViewWatcher.watch_view(watcher, 0)
+      :ok = LiveViewWatcher.watch_view(watcher, 100)
 
-    assert_receive {:watcher, :timeout}
-  end
+      assert_receive {:watcher, :live_view_redirected, _redirect_data}
+    end
 
-  test "sends :async_process_completed message when async process completes" do
-    {:ok, view_pid} = start_supervised({DummyLiveView, []})
-    {:ok, proxy_pid} = start_supervised({DummyLiveViewTestProxy, []})
-    view = %{pid: view_pid, proxy: fake_proxy(proxy_pid, "async_pids")}
-    {:ok, watcher} = start_supervised({LiveViewWatcher, %{view: view, caller: self()}})
+    test "sends :timeout message when LiveView timeout expires" do
+      {:ok, view_pid} = start_supervised({DummyLiveView, []})
+      {:ok, proxy_pid} = start_supervised({DummyLiveViewTestProxy, []})
+      view = %{pid: view_pid, proxy: fake_proxy(proxy_pid)}
+      {:ok, watcher} = start_supervised({LiveViewWatcher, %{view: view, caller: self()}})
 
-    :ok = LiveViewWatcher.watch_view(watcher, 100)
+      :ok = LiveViewWatcher.watch_view(watcher, 0)
 
-    assert_receive {:watcher, :async_process_completed}
+      assert_receive {:watcher, :timeout}
+    end
+
+    test "sends :async_process_completed message when async process completes" do
+      {:ok, view_pid} = start_supervised({DummyLiveView, []})
+      {:ok, proxy_pid} = start_supervised({DummyLiveViewTestProxy, []})
+      view = %{pid: view_pid, proxy: fake_proxy(proxy_pid, "async_pids")}
+      {:ok, watcher} = start_supervised({LiveViewWatcher, %{view: view, caller: self()}})
+
+      :ok = LiveViewWatcher.watch_view(watcher, 100)
+
+      assert_receive {:watcher, :async_process_completed}
+    end
   end
 
   def fake_view(view_pid, proxy_pid) do
