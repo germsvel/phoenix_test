f581cba4261f3eb86395152d08b7741653405122
Claudio Ortolina <cloud8421@gmail.com>
2025-01-08 09:36:40 +0000

    Start structuring the test
    3 examples (plain assert, tag, id+tag). Examples are run each one with a
    separate session in order to avoid any implicit optimization deriving
    from the reuse of the same session. Note that this cannot be avoided
    when optimizations kick in when calling assert_has/2-3 on the same
    session multiple times.

--- bench/assertions.exs
+++ bench/assertions.exs
@@ -1,14 +1,28 @@
-import PhoenixTest
-
 {:ok, _} = Supervisor.start_link([{Phoenix.PubSub, name: PhoenixTest.PubSub}], strategy: :one_for_one)
 {:ok, _} = PhoenixTest.WebApp.Endpoint.start_link()
 
-conn = Phoenix.ConnTest.build_conn()
+session_setup_fn = fn input ->
+  session = PhoenixTest.visit(Phoenix.ConnTest.build_conn(), "/page/index")
+  {input, session}
+end
 
 Benchee.run(%{
-  "assert_has/2" => fn ->
-    conn
-    |> visit("/page/index")
-    |> assert_has("[data-role='title']")
-  end
+  "PhoenixTest.assert_has/2" => {
+    fn {_input, session} ->
+      PhoenixTest.assert_has(session, "[data-role='title']")
+    end,
+    before_scenario: session_setup_fn
+  },
+  "PhoenixTest.assert_has/3, tag selector" => {
+    fn {_input, session} ->
+      PhoenixTest.assert_has(session, "li", text: "Aragorn")
+    end,
+    before_scenario: session_setup_fn
+  },
+  "PhoenixTest.assert_has/3, id+tag selector" => {
+    fn {_input, session} ->
+      PhoenixTest.assert_has(session, "#multiple-items li", text: "Aragorn")
+    end,
+    before_scenario: session_setup_fn
+  }
 })
