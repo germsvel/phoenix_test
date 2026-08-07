d8803c6f9d9532a22a75e4c4b90a9a6220a1e321
German Velasco <germsvel@gmail.com>
2026-08-07 09:30:37 -0400

    Publish version 0.12.1

--- lib/phoenix_test.ex
+++ lib/phoenix_test.ex
@@ -40,7 +40,7 @@ defmodule PhoenixTest do
   ```elixir
   def deps do
     [
-      {:phoenix_test, "~> 0.12.0", only: :test, runtime: false}
+      {:phoenix_test, "~> 0.12.1", only: :test, runtime: false}
     ]
   end
   ```
